f7604a035ca5d756db896e368d25d0e1576ebe3c
khaledsaid89 <51922041+khaledsaid89@users.noreply.github.com>
2019-07-27 00:24:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("603537828625776660")
 setInterval(function() {
 channel.send(`خدني يارب`);
-}, 10)
+}, 30)
 })
 
 client.login(process.env.BOT_TOKEN);
